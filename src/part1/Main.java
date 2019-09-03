6f992d71030155152042daf5c0c394a425e955ba
Conor Baird <conor.baird@my.jcu.edu.au>
2019-09-03 21:21:00 +1000

    Removed and simplified some loops in the main class

--- src/part1/Main.java
+++ src/part1/Main.java
@@ -3,39 +3,33 @@ package part1;
 public class Main {
     public static void main(String[] args){
         int time = 25;
-        Car car = new Car(2,1);
+        Car car = new Car(4,1);
+        TrafficLight trafficLight = new TrafficLight();
         Road road = new Road();
         int[] roadArray = new int[road.roads];
         for (int i=0; i<road.roads;i++){
             roadArray[i] = road.roadSegements;
         }
         for (int value : roadArray) {
+            trafficLight = new TrafficLight();
             while (time > 0) {
-                TrafficLight trafficlight = new TrafficLight();
-                if (trafficlight.lightColorArray[trafficlight.lightColor].equals("Green")) {
-                    while (time > 0) {
-                        System.out.println(" Car Position " + car.carStartPosition + " " + car.carEndPosition);
-                        if (car.carStartPosition < value) {
-                            car.carStartPosition += 1;
-                            car.carEndPosition += 1;
-                        } else {
-                            break;
-                        }
-                        time -= 1;
+                if (trafficLight.lightColorArray[trafficLight.lightColor].equals("Green")) {
+                    System.out.println(" Car Position " + car.carStartPosition + " " + car.carEndPosition);
+                    if (car.carStartPosition < value) {
+                        car.carStartPosition += 1;
+                        car.carEndPosition += 1;
+                    } else {
+                        break;
                     }
-                    car.carStartPosition -= (value-2);
-                    car.carEndPosition -= (value-2);
                 }
                 else {
-                    System.out.println(" Light Color " + trafficlight.lightColorArray[trafficlight.lightColor]);
+                    trafficLight = new TrafficLight();
+                    System.out.println(" Light Color " + trafficLight.lightColorArray[trafficLight.lightColor]);
                 }
-
                 time -= 1;
             }
+            car.carStartPosition -= (value-car.carLength);
+            car.carEndPosition = 1;
         }
-
-
-
-
     }
 }
