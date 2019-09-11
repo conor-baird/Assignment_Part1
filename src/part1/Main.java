b2b3cfbac40092ad3315cf9ecf8b0fb544f14cb7
Conor Baird <conor.baird@my.jcu.edu.au>
2019-09-12 01:04:10 +1000

    Fixed the traffic light class and implemented a operatorLight method into this class and implemeneted these changes into the main class

--- src/part1/Main.java
+++ src/part1/Main.java
@@ -5,19 +5,21 @@ public class Main {
         int time = 25;
         Car car = new Car(3);
         Road road = new Road(3, new int[]{10, 20, 30});
+        TrafficLight trafficLight = new TrafficLight();
 
         for (int value : road.roadSegements) {
-            TrafficLight trafficLight = new TrafficLight();
+            trafficLight.operatorLight();
             for (int i = 0; i<time; i++) {
-                System.out.println(" Car Position " + car.carStartPosition + "-" + car.carEndPosition+ " "+trafficLight.lightColorArray[trafficLight.lightColor]);
-                if (trafficLight.lightColorArray[trafficLight.lightColor].equals("Green") || car.carStartPosition < value) {
+                System.out.println(" Car Position " + car.carStartPosition + "-" + car.carEndPosition+ " "+trafficLight.lightColour);
+                trafficLight.operatorLight();
+                if (trafficLight.lightColour.equals("Green") || car.carStartPosition < value) {
                     if (car.carStartPosition < value) {
                         car.moveCar();
                     } else {
+
                         break;
                     }
                 }
-                trafficLight.lightColorArray[trafficLight.lightColor] = "Green";
             }
             car.setCarToOriginalPos();
         }
--- src/part1/TrafficLight.java
+++ src/part1/TrafficLight.java
@@ -1,9 +1,20 @@
 package part1;
 import java.util.Random;
 public class TrafficLight {
-    String lightColorArray[] = {"Green","Red"};
-    Random random = new Random();
-    int lightColor = random.nextInt(lightColorArray.length);
+    int percentage;
+    String lightColour;
+
+
+    public void operatorLight(){
+        Random random = new Random();
+        int percentage = random.nextInt(100);
+        if (percentage<90){
+            lightColour = "Red";
+        }
+        else{
+            lightColour = "Green";
+        }
+    }
 
     public TrafficLight(){
     }
