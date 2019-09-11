6af248aec5cdffa1bdbff2d446ef61461fa5b66a
Conor Baird <conor.baird@my.jcu.edu.au>
2019-09-12 01:09:23 +1000

    Implemented traffic light test class that check the operator light method in the traffic light class

---
+++ src/part1/TrafficLightTest.java
@@ -0,0 +1,21 @@
+package part1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrafficLightTest {
+
+    @Test void checkOperatorLight(){
+        TrafficLight trafficLight = new TrafficLight();
+        trafficLight.operatorLight();
+        int percentage = 20;
+        if (percentage<30){
+            assertEquals("Red",trafficLight.lightColour);
+        }
+        else{
+            assertEquals("Green",trafficLight.lightColour);
+        }
+    }
+
+}
