1e72396be92e4d3e0375ae57bd6893c2a0679317
Conor Baird <conor.baird@my.jcu.edu.au>
2019-09-13 12:14:24 +1000

    Traffic Light comments

--- src/part1/TrafficLight.java
+++ src/part1/TrafficLight.java
@@ -1,14 +1,15 @@
 package part1;
 import java.util.Random;
+// This intialises the traffic lights colour and randomly changes it depending on a rnadom number
 public class TrafficLight {
     int percentage;
     String lightColour;
 
-
+    // This method randomly selects a number less than 100 and if this number is less than 30 the light colour becomes Red else it is green
     public void operatorLight(){
         Random random = new Random();
         int percentage = random.nextInt(100);
-        if (percentage<90){
+        if (percentage<30){
             lightColour = "Red";
         }
         else{
